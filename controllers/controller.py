30ce5b5dc5450875a6c7d3a2290421a46a2404f7
saimeerasingh <singhsaimeera@gmail.com>
2021-12-02 14:44:16 +0000

    html added form

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,3 +3,8 @@ from flask import render_template , request
 from models.event_list import *
 from models.event import Event
 
+@app.route('/events')
+def index():
+    return render_template('index.html', title = 'Events', events=events)
+
+    
